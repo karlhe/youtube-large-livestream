0ca67ee8f1f69fb121d0762fa7c4361b9a0ec6ee
Karl He <karl.he@gmail.com>
2021-09-16 23:14:04 -0700

    Fix script not working when navigating to video page from other parts of youtube

--- page.js
+++ page.js
@@ -1,29 +1,50 @@
 // Script to be injected into the page
 (() => {
   const player = document.getElementById('movie_player');
-  const video = player.getElementsByTagName('video')[0];
 
   // This uses undocumented APIs, hopefully they continue to exist
-  const setVideoSize = () => {
+  const setVideoSize = (player) => {
     const { width, height } = player.getPlayerSize();
 
     // Our change will also trigger the mutation observer
     // Make sure we don't infinite loop
     if (width < player.clientWidth || height < player.clientHeight) {
       player.setInternalSize(player.clientWidth, player.clientHeight);
-      console.log('resized', player.getPlayerSize());
     }
   };
 
-  // Don't actually need to look at the mutation, mainly need to observe whether
-  // the page has attempted to resize the video so that we can set it to our
-  // preferred size
-  new MutationObserver((_mutationsList, _observer) => {
-    setVideoSize();
-  }).observe(video, {
-    attributes: true,
-    attributeFilter: ['style'],
-  });
+  const createObserver = (video, player) => {
+    // Don't actually need to look at the mutation, mainly need to observe whether
+    // the page has attempted to resize the video so that we can set it to our
+    // preferred size
+    new MutationObserver((_mutationsList, _observer) => {
+      setVideoSize(player);
+    }).observe(video, {
+      attributes: true,
+      attributeFilter: ['style'],
+    });
+  }
 
-  setVideoSize();
+  if (player && player.getElementsByTagName('video')[0]) {
+    const video = player.getElementsByTagName('video')[0];
+    createObserver(video, player);
+    setVideoSize(player);
+  } else {
+    // Wait for the video element to exist
+    new MutationObserver(function(_mutationsList, _observer) {
+      const player = document.getElementById('movie_player');
+      if (player) {
+        const video = player.getElementsByTagName('video')[0];
+        if (video) {
+          createObserver(video, player);
+          setVideoSize(player);
+          this.disconnect();
+        }
+      }
+    }).observe(document, {
+      subtree: true,
+      childList: true,
+    })
+  }
+
 })();
