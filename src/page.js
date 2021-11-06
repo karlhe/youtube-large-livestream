541e1114d6922554da28b075f34e004d286fbf12
Karl He <karl.he@gmail.com>
2021-11-05 19:56:07 -0700

    Dynamically adjust chat height with JS
    This fixes the issue with the chat being too long in clip view

--- src/page.js
+++ src/page.js
@@ -1,6 +1,7 @@
 // Script to be injected into the page
 (() => {
   const player = document.getElementById('movie_player');
+  const secondary = document.getElementById('secondary');
 
   // This uses undocumented APIs, hopefully they continue to exist
   const setVideoSize = (player) => {
@@ -10,6 +11,22 @@
     // Make sure we don't infinite loop
     if (width < player.clientWidth || height < player.clientHeight) {
       player.setInternalSize(player.clientWidth, player.clientHeight);
+    }
+  };
+
+  const setChatSize = () => {
+    const chat = document.getElementById('chat');
+    if (chat) {
+      // This looks weird, but chat's offsetParent actually changes depending on
+      // whether or not chat is enabled, throwing off the calculation
+      const height = window.innerHeight - chat.offsetTop - chat.offsetParent.offsetTop - 24;
+      if (!chat.hasAttribute('collapsed') && height > 720) {
+        // Only set if it would actually expand chat
+        chat.style.height = `${height}px`;
+      } else {
+        // Otherwise, remove styling
+        chat.style.height = '';
+      }
     }
   };
 
@@ -28,11 +45,22 @@
         setVideoSize(player);
       }
     }, false);
-  }
+  };
+
+  const createChatObserver = () => {
+    // This observer is pretty broad, might be room to optimize this?
+    new MutationObserver((_mutationsList, _observer) => {
+      setChatSize();
+    }).observe(secondary, {
+      attributes: true,
+      subtree: true,
+    });
+  };
 
   if (player && player.getElementsByTagName('video')[0]) {
     const video = player.getElementsByTagName('video')[0];
     createObserver(video, player);
+    createChatObserver();
     setVideoSize(player);
   } else {
     // Wait for the video element to exist
@@ -42,6 +70,7 @@
         const video = player.getElementsByTagName('video')[0];
         if (video) {
           createObserver(video, player);
+          createChatObserver();
           setVideoSize(player);
           this.disconnect();
         }
@@ -49,7 +78,7 @@
     }).observe(document, {
       subtree: true,
       childList: true,
-    })
+    });
   }
 
 })();
